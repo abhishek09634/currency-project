de16cff396b314e26312718f4a541d7ee04687d5
abhishek09634 <abhisheknehe@gmail.com>
2023-01-29 23:22:24 +0530

    1 binaries uploaded

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 const axios = require('axios');
 const PropertiesReader = require('properties-reader');
-const prop = PropertiesReader('./application.properties');
-const express = require("express");
+const prop = PropertiesReader('application.properties');
+const express = require('express');
 var app = express();
 
-const HTTP_PORT = 8000
-app.listen(HTTP_PORT, () => {
-    console.log("Server is listening on port " + HTTP_PORT);
-});
+// const HTTP_PORT = 8000;
+// app.listen(HTTP_PORT, () => {
+//   console.log('Server is listening on port ' + HTTP_PORT);
+// });
 
 var sqlite3 = require('sqlite3').verbose();
 
@@ -41,7 +41,17 @@ async function pushCurrencyDataFunc() {
   let CNY = res.rates.CNY;
   let CURRENT_DATE_TIME = new Date();
 
-  console.log(CURRENT_DATE_TIME.toLocaleString()+ ' : '+USD + ' : ' + EUR + ' : ' + GBP + ' : ' + CNY);
+  console.log(
+    CURRENT_DATE_TIME.toLocaleString() +
+      ' : ' +
+      USD +
+      ' : ' +
+      EUR +
+      ' : ' +
+      GBP +
+      ' : ' +
+      CNY
+  );
 
   const DBSOURCE = 'currency_rates_db.sqlite';
 
@@ -61,28 +71,27 @@ async function pushCurrencyDataFunc() {
             eur INTEGER, 
             gbp INTEGER, 
             cny INTEGER
-            )`);
-        console.log("inserting values.....");    
-        db.run('INSERT INTO currency_rates (timestamp, usd, eur, gbp, cny) VALUES (?,?,?,?,?)', [CURRENT_DATE_TIME, USD, EUR, GBP, CNY]);
+            )`
+        );
+        console.log('inserting values.....');
+        db.run(
+          'INSERT INTO currency_rates (timestamp, usd, eur, gbp, cny) VALUES (?,?,?,?,?)',
+          [CURRENT_DATE_TIME, USD, EUR, GBP, CNY]
+        );
       });
-     
     }
   });
 
-  app.get("/currencies", (req, res, next) => {
-    db.get("SELECT * FROM currency_rates", [], (err, rows) => {
-        if (err) {
-            res.status(400).json({ "error": err.message });
-            return;
-        }
-        res.status(200).json({ rows });
-    });
-    db.close();
-  });
-
-
+  // app.get("/currencies", (req, res, next) => {
+  //   db.get("SELECT * FROM currency_rates", [], (err, rows) => {
+  //       if (err) {
+  //           res.status(400).json({ "error": err.message });
+  //           return;
+  //       }
+  //       res.status(200).json({ rows });
+  //   });
+  //   db.close();
+  // });
 }
 
 pushCurrencyDataFunc();
-
-
